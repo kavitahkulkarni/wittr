64c2b87083b2f02cdca33f73b72bfa7197238cbe
Kavita Kulkarni <kavitahkulkarni@gmail.com>
2016-10-26 11:37:25 -0700

    solution for installing new cache safely bye deleting the old one

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,31 +1,44 @@
+var staticCacheName = 'wittr-static-v2'
+
 self.addEventListener('install', function(event) {
   event.waitUntil(
-    // TODO: change the site's theme, eg swap the vars in public/scss/_theme.scss
-    // Ensure at least $primary-color changes
-    // TODO: change cache name to 'wittr-static-v2'
-    caches.open('wittr-static-v1').then(function(cache) {
-      return cache.addAll([
-        '/',
-        'js/main.js',
-        'css/main.css',
-        'imgs/icon.png',
-        'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff',
-        'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
-      ]);
-    })
+	// TODO: change the site's theme, eg swap the vars in public/scss/_theme.scss
+	// Ensure at least $primary-color changes
+	// TODO: change cache name to 'wittr-static-v2'
+	caches.open(staticCacheName).then(function(cache) {
+	  return cache.addAll([
+		'/',
+		'js/main.js',
+		'css/main.css',
+		'imgs/icon.png',
+		'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff',
+		'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
+	  ]);
+	})
   );
 });
 
 self.addEventListener('activate', function(event) {
   event.waitUntil(
-    // TODO: remove the old cache
+	// TODO: remove the old cache
+  	caches.keys().then(function(cacheNames) {
+  		console.log(cacheNames);
+  		return Promise.all(
+  		  cacheNames.filter(function(cacheName) {
+  			return cacheName.startsWith('wittr-') && cacheName != staticCacheName;
+  		  }).map(function(cacheName) {
+  		  	console.log("deleted " + cacheName);
+  				return caches.delete(cacheName);
+  		  })
+  		);
+  	})
   );
 });
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
+	caches.match(event.request).then(function(response) {
+	  return response || fetch(event.request);
+	})
   );
 });
