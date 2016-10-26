bb71b76bc17d268317fd0c60c9553466317e1bad
Kavita Kulkarni <kavitahkulkarni@gmail.com>
2016-10-26 15:59:58 -0700

    Solution to update notification quiz

--- public/js/main/IndexController.js
+++ public/js/main/IndexController.js
@@ -20,18 +20,52 @@ IndexController.prototype._registerServiceWorker = function() {
     // TODO: if there's no controller, this page wasn't loaded
     // via a service worker, so they're looking at the latest version.
     // In that case, exit early
-
+    if (!navigator.serviceWorker.controller) {
+      return;
+    }
     // TODO: if there's an updated worker already waiting, call
     // indexController._updateReady()
+    if(reg.waiting) {
+      indexController._updateReady();
+      return;
+    }
 
     // TODO: if there's an updated worker installing, track its
     // progress. If it becomes "installed", call
     // indexController._updateReady()
-
+    if(reg.installing) {
+      reg.installing.addEventListener('statechange', function(){
+        if(reg.state == 'installed') {
+          console.log("Update is ready/installed");
+          indexController._updateReady();
+          return;
+        }
+      });
+//      indexController._trackInstalling(reg.installing);
+//      return;
+    }
     // TODO: otherwise, listen for new installing workers arriving.
     // If one arrives, track its progress.
     // If it becomes "installed", call
     // indexController._updateReady()
+    reg.addEventListener('updatefound', function() {
+      reg.installing.addEventListener('statechange', function(){
+        if(reg.state == 'installed') {
+          console.log("New update is ready/installed");
+          indexController._updateReady();
+        }
+      });
+//      indexController._trackInstalling(reg.installing);
+    });
+  });
+};
+
+IndexController.prototype._trackInstalling = function(worker) {
+  var indexController = this;
+  worker.addEventListener('statechange', function() {
+    if (worker.state == 'installed') {
+      indexController._updateReady();
+    }
   });
 };
 
